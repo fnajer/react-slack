17da1e0d4a34c201cde0ab025fc12572a5fed775
fnajer <lexactr@gmail.com>
2018-12-06 22:44:32 +0300

    Create register form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { Grid, Header, Segment, Icon, Form, Message, Button } from 'semantic-ui-react';
 
 class Register extends React.Component {
+  state = {}
+
+  handleChange = () => {}
+
   render() {
     return (
-      <div>
-        Register
-      </div>
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange"/>
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input fluid name="username" icon="user" iconPosition="left"
+              placeholder="Username" onChange={this.handleChange} type="text" />
+
+              <Form.Input fluid name="email" icon="mail" iconPosition="left"
+              placeholder="Email Address" onChange={this.handleChange} type="text" />
+
+              <Form.Input fluid name="password" icon="lock" iconPosition="left"
+              placeholder="Password" onChange={this.handleChange} type="password" />
+
+              <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
+              placeholder="Password Confirmation" onChange={this.handleChange} type="password" />
+
+              <Button color="orange" fluid size="large">Submit</Button>
+            </Segment>
+          </Form>
+          <Message>Already a user? <Link to="/login">Login</Link></Message>
+        </Grid.Column>
+      </Grid>
     );
   }
 };
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var config = {
+  apiKey: "AIzaSyB6mLLhf9MckCBnI9Gkc4VVAY58jGtdvV4",
+  authDomain: "react-slack-304c1.firebaseapp.com",
+  databaseURL: "https://react-slack-304c1.firebaseio.com",
+  projectId: "react-slack-304c1",
+  storageBucket: "react-slack-304c1.appspot.com",
+  messagingSenderId: "344940897350"
+};
+firebase.initializeApp(config);
