2bd05fcb5d0a781dfb5b50859f5ae72763493dbe
fnajer <lexactr@gmail.com>
2018-12-16 15:48:23 +0300

    Show errors, caught on server

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -66,7 +66,7 @@ class Register extends React.Component {
         })
         .catch(err => {
           console.log(err);
-          this.setState({ loading: false });
+          this.setState({ errors: this.state.errors.concat(err), loading: false });
         });
     }
   }
