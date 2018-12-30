68537e9b6d818de399dce831cdd2e08db4a08ac9
fnajer <lexactr@gmail.com>
2018-12-30 20:33:18 +0300

    Listen messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -7,7 +7,34 @@ import MessageForm from './MessageForm';
 class Messages extends React.Component {
   state={
     messagesRef: firebase.database().ref('messages'),
+    messages: [],
+    messageLoading: true,
   }
+
+  componentDidMount() {
+    const { currentChannel, currentUser } = this.props;
+
+    if (currentChannel && currentUser) {
+      this.addListeners(currentChannel.id);
+    }
+  }
+
+  addListeners = channelId => {
+    this.addMessageListener(channelId);
+  }
+
+  addMessageListener = channelId => {
+    const loadedMessages = [];
+
+    this.state.messagesRef.child(channelId).on('child_added', snap => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messageLoading: false,
+      })
+    });
+  }
+
 
   render() {
     const { messagesRef } = this.state;
