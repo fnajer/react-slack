253dc69ef4ef69a979726892f70339aa4b37b1a2
fnajer <lexactr@gmail.com>
2019-01-31 14:41:06 +0300

    Get choosen colors and put on state

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { Sidebar, Menu, Divider, Button, Modal, Label, Icon } from 'semantic-ui-react';
+import { Sidebar, Menu, Divider, Button, Modal, Label, Icon, Segment } from 'semantic-ui-react';
 import { SliderPicker } from 'react-color';
 
 class ColorPanel extends React.Component {
   state={
     modal: false,
+    primary: "#4040bf",
+    secondary: "#4083bf",
   }
 
   openModal = () => this.setState({ modal: true });
 
   closeModal = () => this.setState({ modal: false });
 
+  handleChangePrimary = color => this.setState({ primary: color.hex });
+
+  handleChangeSecondary = color => this.setState({ secondary: color.hex });
+
   render() {
-    const { modal } = this.state;
+    const { modal, primary, secondary } = this.state;
 
     return (
       <Sidebar
@@ -30,10 +36,15 @@ class ColorPanel extends React.Component {
         <Modal basic open={modal} onClose={this.closeModal}>
           <Modal.Header>Choose App Colors</Modal.Header>
           <Modal.Content>
-            <Label content="Primary Color"/>
-            <SliderPicker/>
-            <Label content="Secondary Color"/>
-            <SliderPicker/>
+            <Segment>
+              <Label content="Primary Color"/>
+              <SliderPicker color={primary} onChange={this.handleChangePrimary}/>
+            </Segment>
+
+            <Segment>
+              <Label content="Secondary Color"/>
+              <SliderPicker color={secondary} onChange={this.handleChangeSecondary}/>
+            </Segment>
           </Modal.Content>
           <Modal.Actions>
             <Button color="green" inverted>
