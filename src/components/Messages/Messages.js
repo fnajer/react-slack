aa6cf4eb28a0424bb2a1cb59c7d17d2833b9bd94
fnajer <lexactr@gmail.com>
2019-02-03 16:59:05 +0300

    Add Typing component with animation

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -6,6 +6,7 @@ import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
+import Typing from './Typing';
 
 class Messages extends React.Component {
   state={
@@ -199,6 +200,9 @@ class Messages extends React.Component {
         <Segment>
           <Comment.Group className={progressBar ? 'messages__progress' : 'messages'}>
             {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <span className='user__typing'>mool is typing</span> <Typing/>
+            </div>
           </Comment.Group>
         </Segment>
 
